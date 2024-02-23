e7afe807ecaaf09520d67a492d595bfc282d4966
FarahAlmas <160884624+FarahAlmas@users.noreply.github.com>
2024-02-23 12:12:39 +0500

    Add files via upload

---
+++ 10.Adding_comment.ts
@@ -0,0 +1,6 @@
+
+
+//program written by:farah
+//current date :20-2-2024
+//this program will write Hello World
+console.log('Hello World');
---
+++ Adding_comment_2.ts
@@ -0,0 +1,4 @@
+//program written by:farah
+//current date :20-2-2024
+//this program will add two digits.
+console.log(2+2);
