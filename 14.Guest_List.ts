1c742f55ab8039b0e0248aa03e4929700131ab85
FarahAlmas <160884624+FarahAlmas@users.noreply.github.com>
2024-02-23 12:19:21 +0500

    Add files via upload

---
+++ 14.Guest_List.ts
@@ -0,0 +1,5 @@
+
+let Guest_List :string[] = ['jon','rich','liza'];
+for(let i=0; i<Guest_List.length; i++){
+     console.log('Dear,' +Guest_List[i] +',\n\nIt is our pleasure to invite you in our party.\n\nThank You!')
+}
