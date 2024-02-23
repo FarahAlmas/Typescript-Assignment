f0b9bc3b502d3a913f8a5ee3994df349558bc6bd
FarahAlmas <160884624+FarahAlmas@users.noreply.github.com>
2024-02-23 12:16:50 +0500

    Add files via upload

---
+++ 12.Greeting.ts
@@ -0,0 +1,6 @@
+let names :string[] =['Almas','Azlan','Asad','Fahad'];
+
+const message : string = 'welcome to typescript class: '
+for(let i=0; i<names.length; i++){
+    console.log(message + names[i]);
+}
