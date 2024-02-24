2da845764ed7df7846fad5a7acc396ae8f9fbddb
FarahAlmas <160884624+FarahAlmas@users.noreply.github.com>
2024-02-24 21:29:16 +0500

    Add files via upload

---
+++ 20.Countries.ts
@@ -0,0 +1,9 @@
+//Think of something tou cloud store in a array.
+let countries : string[] = ['Pakistan','India','Bangladesh','China','Russia'];
+console.log('list of countries '+countries);
+//write a programme that create a list containing these items.
+let country :string[] =[];
+country .push('france');
+country .push('itlay');
+country .push('denmark');
+console.log(country);
