736eae7e8b22879206709e07dc6f732858e34f97
FarahAlmas <160884624+FarahAlmas@users.noreply.github.com>
2024-02-26 09:46:16 +0500

    Add files via upload

---
+++ 21.Typescript_object.ts
@@ -0,0 +1,17 @@
+//write the programme that create object containing these item.
+//datatype of person object.
+interface person {
+    name:string,
+    age:number,
+    roll_number:number,
+    is_student:boolean,
+}
+//person object.    
+let person: person = {
+    name:'Maha',
+    age: 25,
+    roll_number:1234,
+    is_student:true,
+}
+//print person.
+console.log(person);
