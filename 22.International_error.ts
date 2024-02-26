ffb6a997f0a77252241c00857249158b41941eb8
FarahAlmas <160884624+FarahAlmas@users.noreply.github.com>
2024-02-26 09:47:59 +0500

    Add files via upload

---
+++ 22.International_error.ts
@@ -0,0 +1,8 @@
+//if you haven't received an arry index error in one of your programs yet, try to make one happen.
+//create array.
+let foodItem :string[]=['pizza','burger','fries','donuts'];
+//accessing array by index.
+console.log(foodItem[7]);
+//make sure you correct the error before closing the program.
+//correct the error.
+console.log(foodItem);
