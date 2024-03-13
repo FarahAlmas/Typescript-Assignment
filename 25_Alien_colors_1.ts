10be75fba271a8c8020b6027a0890d169f9d0349
FarahAlmas <160884624+FarahAlmas@users.noreply.github.com>
2024-03-13 09:56:26 +0500

    Add files via upload

---
+++ 25_Alien_colors_1.ts
@@ -0,0 +1,12 @@
+//create a variable called alien-color.
+
+let alien_color :string = 'green';
+if(alien_color === 'green'){ 
+    console.log("player just earned 5 points!");
+}
+//write one version of this program that passes the if test another that
+// fail. (the version that fail will have no output.)
+alien_color = 'red';
+if(alien_color === 'green'){ 
+    console.log("player just earned 5 points!");
+}
