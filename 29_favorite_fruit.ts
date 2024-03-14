eae18c47b72fa524d788816bcc31746f118ac32d
FarahAlmas <160884624+FarahAlmas@users.noreply.github.com>
2024-03-14 11:11:31 +0500

    Add files via upload

---
+++ 29_favorite_fruit.ts
@@ -0,0 +1,18 @@
+//make a array for favorite fruits.
+ let favorite_fruit : string[]=['apple','mango', 'banana'];
+
+ if(favorite_fruit.includes('apple')){
+    console.log('i like apple');
+ }
+ if(favorite_fruit.includes('mango')){
+    console.log('i like mango');
+ }
+ if(favorite_fruit.includes('banana')){
+    console.log('i like banana');
+ }
+ if(favorite_fruit.includes('kiwi')){
+    console.log('i like kiwi');
+ }
+ if(favorite_fruit.includes('grapes')){
+    console.log('i like grapes');
+ }
