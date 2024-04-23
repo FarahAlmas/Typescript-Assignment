6e53b03dabe9c6439d2873d17d7f947044ec70bf
FarahAlmas <160884624+FarahAlmas@users.noreply.github.com>
2024-04-23 21:24:15 +0500

    Add files via upload

---
+++ 31_No_user.ts
@@ -0,0 +1,7 @@
+let userName: string[] =['sana','sara','mina','rina','rana'];
+if(userName.length === 0){
+    console.log(`we need to find some users!`);
+}else{
+    userName = [];
+    console.log(`all user names have been removed.` + userName.length);
+}
