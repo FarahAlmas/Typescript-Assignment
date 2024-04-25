b570a81da06468b6e8597d9b3642a316fb579b92
FarahAlmas <160884624+FarahAlmas@users.noreply.github.com>
2024-04-25 11:58:28 +0500

    Add files via upload

---
+++ 35_Animals.ts
@@ -0,0 +1,7 @@
+// make a array of animals which use in loop.
+
+let animals:string[] = ['dog', 'cat', 'turtle'];
+for(let animal of animals){
+    console.log(`A ${animal} would make a great pet.`)
+};
+console.log(`Any of these animals would make a great pet!`)
