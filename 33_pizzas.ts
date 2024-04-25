83c66ace228fe30720d90879fc8ea4dd845bdc8f
FarahAlmas <160884624+FarahAlmas@users.noreply.github.com>
2024-04-25 11:53:15 +0500

    Add files via upload

---
+++ 33_pizzas.ts
@@ -0,0 +1,8 @@
+//make a arry 
+//then one by one run in cli to my console message
+
+let pizzas:string[]= ['pepperoni','chicken tikka','fajita','creamy melt'];
+for (let pizza of pizzas){
+    console.log(`i like ${pizza} pizzas.`)
+};
+console.log(`i really love pizza!`)
