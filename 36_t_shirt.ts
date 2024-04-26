0ab1b5b02378afa2a1aa2dcb70211dadfb5c6a4b
FarahAlmas <160884624+FarahAlmas@users.noreply.github.com>
2024-04-26 15:02:21 +0500

    Add files via upload

---
+++ 36_t_shirt.ts
@@ -0,0 +1,8 @@
+//make a funtion
+
+function make_shirt(size:string, text:string){
+    console.log(`create a ${size} shirt with the message:${text}`);
+}
+make_shirt("small","my think");
+make_shirt("medium","hello");
+make_shirt("large","how are you");
