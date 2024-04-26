24fedac2ee333c8f672e8a1ee5ded4adb295fb36
FarahAlmas <160884624+FarahAlmas@users.noreply.github.com>
2024-04-26 15:22:21 +0500

    Add files via upload

---
+++ 39_city_names.ts
@@ -0,0 +1,8 @@
+//make function
+ function city_country(city:string,country:string){
+
+    return `${city},${country}`;
+ }
+console.log(city_country('karachi','pakistan'));
+console.log(city_country('dehli','india'));
+console.log(city_country('tokyo','japan'));
